c16968677afb3fc0436dc35b5089aa059bc9f1f4
Anaramos152 <jhandyramos793308@gmail.com>
2026-08-24 21:02:27 -0500

    modified:   backend/controllers/usuario/categorias.controller.js         modified:   backend/index.js         modified:   frontend/.dart_tool/dartpad/web_plugin_registrant.dart         modified:   frontend/.dart_tool/package_config.json         modified:   frontend/.dart_tool/package_graph.json         modified:   frontend/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java         modified:   frontend/ios/Runner/GeneratedPluginRegistrant.m

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,7 @@ const PORT = process.env.PORT || 3000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
-        console.log('MongoDB conectado correctamente ✔️');
+        console.log('MongoDB conectado correctamente ✅');
 
         app.listen(PORT, () => {
             console.log(`Servidor funcionando en http://localhost:${PORT}🚀`);
