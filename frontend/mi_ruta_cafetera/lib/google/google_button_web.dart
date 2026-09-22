7843017515ef770be2cd8618c8eeb290ae19f00d
dev-emfel <mauro.n120@gmail.com>
2026-09-21 20:15:51 -0500

    Recupera integracion original de Google Sign-In

---
+++ frontend/mi_ruta_cafetera/lib/google/google_button_web.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in_web/web_only.dart' as google_web;
+
+Widget crearBotonGoogleWeb() {
+  return google_web.renderButton(
+    configuration: google_web.GSIButtonConfiguration(
+      type: google_web.GSIButtonType.standard,
+      theme: google_web.GSIButtonTheme.outline,
+      size: google_web.GSIButtonSize.large,
+      text: google_web.GSIButtonText.continueWith,
+      shape: google_web.GSIButtonShape.pill,
+      logoAlignment: google_web.GSIButtonLogoAlignment.center,
+      minimumWidth: 300,
+      locale: 'es',
+    ),
+  );
+}
